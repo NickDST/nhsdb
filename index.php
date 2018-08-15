c2259cebc1fb201bdea7cb63eff17666d8a80a71
Jack Wang <jack2020154@concordiashanghai.org>
2018-08-15 18:33:00 +0800

    Commit?

--- index.php
+++ index.php
@@ -1,18 +1,67 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>index</title>
-</head>
-	
-	<a href="login.php">Login</a>
-	<br>
-	<a href="register.php">Register</a>
-	<br>
-	<a href="biologytutorrequest.php">submit bio request</a>
-	<br>
-	<a href="requestcal2.php">See all</a>
+<title>index</title>	
+	<head>
+		<title>Projection by TEMPLATED</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body>
 
-<body>
-</body>
-</html>
+		<!-- Header -->
+			<header id="header">
+				<div class="inner">
+					<a href="index.html" class="logo"><strong>Projection</strong> by TEMPLATED</a>
+					<nav id="nav">
+						<a href="login.php">Admin Login</a>
+					</nav>
+					<a href="#navPanel" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+				</div>
+			</header>
+
+		<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<header>
+						<h1>NHS Website, idk change the name nick</h1>
+					</header>
+
+					<div class="flex ">
+
+						<div>
+							<a href="requestcal2.php"><h3>NHS Calendar</h3></a>
+							<p>See currently scheduled</p>
+							<p>events, activities, and more!</p>
+						</div>
+
+						<div>
+							<a href="biologytutorrequest.php"><h3>Biology Tutoring</h3></a>
+							<p>Struggling? Find </p>
+							<p>someone to help!</p>
+						</div>
+
+						
+
+					</div>
+
+					<footer>
+						<a href="register.php" class="button">Register</a>
+						<p></p>
+						<p>No account? Register here!</p>
+					</footer>
+				</div>
+			</section>
+
+
+		<!-- Three -->
+			
+
+		<!-- Footer -->
+			
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
