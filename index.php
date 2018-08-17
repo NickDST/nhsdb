c3cf1bffbeaa08a1a00db99126af8aa2d44ce2cf
NickDST <learningnickk@gmail.com>
2018-08-17 19:11:11 +0800

    added useless but cool wheel
    desc

--- index.php
+++ index.php
@@ -12,6 +12,31 @@
   
       <link rel="stylesheet" href="css/style.css">
 
+<!--testing out some different wheel stuff	-->
+	
+			<meta name="description" content="Circular Navigation Styles - Building a Circular Navigation with CSS Transforms | Codrops " />
+		<meta name="keywords" content="css transforms, circular navigation, round navigation, circular menu, tutorial" />
+		<meta name="author" content="Sara Soueidan for Codrops" />
+		<link rel="shortcut icon" href="../favicon.ico">
+<!--		<link rel="stylesheet" type="text/css" href="css/normalize.css" />-->
+<!--		<link rel="stylesheet" type="text/css" href="css/demo.css" />-->
+		<link rel="stylesheet" type="text/css" href="css/component2.css" />
+		<script src="js/modernizr-2.6.2.min.js"></script>
+
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-7243260-2']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>
+	
+	
+	
+	
   
 </head>
 
@@ -22,14 +47,52 @@
     <div class="strip__content">
       <h1 class="strip__title" data-name="Lorem">Science</h1>
       <div class="strip__inner-text">
+<!--		    <p>We offer</p>-->
         <h2>Science Tutoring Services</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia sapiente deserunt consectetur, quod reiciendis corrupti quo ea aliquid! Repellendus numquam quo, voluptate. Suscipit soluta omnis quibusdam facilis, illo voluptates odit!</p>
-		  <a href="biologytutorrequest.php">Biology</a>
+<!--        <p>We offer tutoring science fields.  </p>-->
+<!--		  <a href="biologytutorrequest.php">Biology</a>-->
         <p>
-          <a href="https://twitter.com/ettrics" target="_blank"><i class="fa fa-twitter"></i></a>
+<!--          <a href="https://twitter.com/ettrics" target="_blank"><i class="fa fa-twitter"></i></a>-->
         </p>
+		  <br>
+		  <br>
+		  <br>
+		  <br>
+		  <br>
+		  <br>
+		  <br>
+		  <br>
+		  <br>
+		
+		  
+
+		  		<!-- Start Nav Structure -->
+				<button class="cn-button" id="cn-button">Menu</button>
+				<div class="cn-wrapper" id="cn-wrapper">
+					<ul>
+						<li><a href="#"><span></span></a></li>
+						<li><a href="#"><span>SNHS</span></a></li>
+						<li><a href="#"><span>Physics</span></a></li>
+						<li><a href="biologytutorrequest.php"><span>Biology</span></a></li>
+						<li><a href="#"><span>Chemistry</span></a></li>
+						<li><a href="#"><span>Comp Sci</span></a></li>
+						<li><a href="#"><span></span></a></li>
+					 </ul>
+				</div>
+			  
+			  
+				<!-- End of Nav Structure -->
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
       </div>
       
+		
     </div>
   </article>
   <article class="strips__strip">
@@ -88,7 +151,8 @@
   
 
     <script  src="js/index.js"></script>
-
+    <script src="js/polyfills.js"></script>
+	<script src="js/demo2.js"></script>
 
 
 
